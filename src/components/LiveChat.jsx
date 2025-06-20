0180c7687cda381fc09c8fa02203e48f4227d260
Jintops <jintops2003@gmail.com>
2025-06-21 02:36:13 +0530

    live chat

---
+++ src/components/LiveChat.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import ChatMessage from './ChatMessage'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage } from '../utils/chatSlice';
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
+
+const LiveChat = () => {
+
+const ChatMessages=useSelector(store=>store.chat.messages)
+
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+
+    const i=setInterval(()=>{
+     dispatch(addMessage({name:generateRandomName(), 
+        message:makeRandomMessage(15)}))
+    },2000);
+
+    return ()=>clearInterval(i);
+
+  },[])
+
+  return (
+    <div className='border border-black mx-8 h-[750px] p-2 bg-slate-100 rounded-xl overflow-scroll '>
+        <h1 className='px-2 text-xl  border-t-1'>Top chat</h1>
+        
+        {
+            ChatMessages.map((c)=>(
+                 <ChatMessage name={c.name} message={c.message} />
+            ))
+        }
+      
+         
+    </div>
+  )
+}
+
+export default LiveChat
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { closeMenu, toggleMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
 import CommentsContainer from './CommentsContainer';
+import LiveChat from './LiveChat';
 
 const WatchPage = () => {
    const [searchParams]=useSearchParams();
@@ -13,12 +14,17 @@ const WatchPage = () => {
     }, [])
 
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col w-full'>
+            <div className='flex '>
         <div className='px-20 p-2'>
-          <iframe className='rounded-xl' width="900" height="550" src={"https://www.youtube.com/embed/"+searchParams.get("v")}
+          <iframe className='rounded-xl' width="1150" height="750" src={"https://www.youtube.com/embed/"+searchParams.get("v")}
           title="YouTube video player" frameBorder="0" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
            referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+        <div className='w-full'>
+            <LiveChat/>
+        </div>
         </div>
         <CommentsContainer/>
         </div>
